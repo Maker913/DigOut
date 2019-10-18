b006bad3a35354a8100feb009135ff81c6a96dc8
sakuma <syoma.0415@gmail.com>
2019-10-18 13:49:41 +0900

    [New] ステージ読み込み書き込みテスト

---
+++ DigOut/Assets/Scenes/Sakuma/Script/NewBehaviourScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject stageList;
+    [SerializeField]
+    private GameObject[] preList;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string text="";
+
+        for(int i=0;i<stageList.transform.GetChildCount(); i++)
+        {
+            text += stageList.transform.GetChild(i).transform.tag+",";
+            text += stageList.transform.GetChild(i).transform.position.x.ToString ()+",";
+            text += stageList.transform.GetChild(i).transform.position.y.ToString() ;
+            text += ";";
+        }
+
+        textSave(text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void textSave(string txt)
+    {
+        StreamWriter sw = new StreamWriter("Assets/Resources/Scenarios/LogData.txt", false ); 
+        sw.WriteLine(txt);
+        sw.Flush();
+        sw.Close();
+    }
+
+}
---
+++ DigOut/Assets/Scenes/Sakuma/Script/lodeTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class lodeTest : MonoBehaviour
+{
+    public string text;
+    public string[] m_scenarios;
+
+    public GameObject[] preList;
+    [SerializeField]
+    private GameObject p;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ReadText("LogData");
+        m_scenarios=text.Split(new string[] { ";" }, System.StringSplitOptions.None);
+        for (int i = 0; i < m_scenarios.Length -1; i++)
+        {
+            string[] pass= m_scenarios[i].Split(new string[] { "," }, System.StringSplitOptions.None);
+            Instantiate(preList[int.Parse(pass[0])],new Vector3 (float.Parse(pass[1]), float.Parse(pass[2]),0),Quaternion.identity ,p.transform );
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ReadText(string fileName)
+    {
+        var scenarioText = Resources.Load<TextAsset>("Scenarios/" + fileName);
+        if (scenarioText == null)
+        {
+            Debug.LogError("シナリオファイルが見つかりません。");
+            return;
+        }
+
+        text = scenarioText.text;//.Split(new string[] { "\n" }, System.StringSplitOptions.None);
+        Resources.UnloadAsset(scenarioText);
+    }
+
+}

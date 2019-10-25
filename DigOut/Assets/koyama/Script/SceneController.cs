f939d8aab41ed8f6dcf8615aafbf751fb3a02828
koyama <>
2019-10-25 11:21:32 +0900

    [new] 仮タイトルと仮シーンマネージャー

---
+++ DigOut/Assets/koyama/Script/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController Instance;
+    public enum SceneName
+    {
+        Title = 0,
+        Main,
+        Result
+    }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void SceneChange(SceneName scene)
+    {
+        SceneManager.LoadScene((int)scene);
+    }
+}
---
+++ DigOut/Assets/koyama/Script/TitleController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleController : MonoBehaviour
+{
+    [SerializeField]
+    private SceneController.SceneName scene;
+    void Start()
+    {
+        
+    }
+    public void Button()
+    {
+        SceneController.Instance.SceneChange(scene);
+    }
+}

71df3a5136f10cf87b6ae823d52bbdd061d07ac3
natuto1998 <kouta.taka.0811.15@gmil.com>
2019-11-02 12:02:22 +0900

    a

---
+++ DigOut/Assets/Sakuma/Script/StageMake/StageName.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StageName : MonoBehaviour
+{
+    InputField inputField;
+
+    public Text stagename;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DisplayText()
+    {
+        stagename.GetComponent<Text>().text = inputField.text;
+    }
+}

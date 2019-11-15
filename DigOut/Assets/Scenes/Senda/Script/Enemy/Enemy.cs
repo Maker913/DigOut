f3380fb4c8f54d1a296853dd4af58557168fbf7f
aa <a@a>
2019-11-15 13:50:05 +0900

    さぐようちゅう

---
+++ DigOut/Assets/Scenes/Senda/Script/Enemy/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[ RequireComponent (typeof (NavMeshAgent))] public class Enemy : Controller2D
+{
+    public float DefMoveSpeed;
+
+    public float MoveSpeed;
+
+    private Rigidbody2D rigidbody2d;
+
+    private Transform target;
+
+    private NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

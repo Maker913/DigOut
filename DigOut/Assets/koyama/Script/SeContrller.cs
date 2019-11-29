00c5ee51362dca41e8cd8d1d32b21342a8c2190a
koyama <>
2019-11-29 13:53:13 +0900

    age

---
+++ DigOut/Assets/koyama/Script/SeContrller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeContrller : SoundModel
+{
+    private AudioSource audioSource;
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    //再生
+    public void PlaySE(int number)
+    {
+        audioSource.PlayOneShot(audioClips[number]);
+    }
+    //停止
+    public void StopSE()
+    {
+        audioSource.Stop();
+    }
+}
---
+++ DigOut/Assets/koyama/Script/SoundController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public static SoundController Instance;
+    private BgmContoller bgmContoller;
+    private SeContrller SeContrller;
+    public enum BgmName
+    {
+
+    }
+    public enum SeName
+    {
+
+    }
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        bgmContoller = GetComponentInChildren<BgmContoller>();
+        SeContrller = GetComponentInChildren<SeContrller>();
+    }
+    /// <summary>
+    /// BGM再生
+    /// <para>SoundController.Instance.PlayBGM("AudioManager.BgmName.名前") </para>
+    /// </summary>
+    /// <param name="name"></param>
+    public void PlayBGM(BgmName name)
+    {
+        bgmContoller.PlayBgm((int)name);
+    }
+    //BGM停止
+    public void StopBGM()
+    {
+        bgmContoller.StopBgm();
+    }
+    /// <summary>
+    /// SE再生
+    /// <para>SoundController.Instance.PlaySE("AudioManager.SeName.名前") </para>
+    /// </summary>
+    /// <param name="name"></param>
+    public void PlaySE(SeName name)
+    {
+        SeContrller.PlaySE((int)name);
+    }
+    //SE停止
+    public void StopSE()
+    {
+        SeContrller.StopSE();
+    }
+}

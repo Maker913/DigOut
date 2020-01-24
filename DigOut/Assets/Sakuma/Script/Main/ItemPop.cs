93eed6d6c4ca0c99859bb22e8100b3e3b18257e0
xiaohaotin <>
2020-01-24 13:41:18 +0900

    [Update]

---
+++ DigOut/Assets/Sakuma/Script/Main/ItemPop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ItemPop : MonoBehaviour
+{
+    [SerializeField]
+    Text dy;
+    [SerializeField]
+    Text heel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MainStateInstance.mainStateInstance.mainState.gameMode ==MainStateInstance.GameMode.Play)
+        {
+            dy.text = ItemList.itemList.dynamite.ToString();
+            heel.text = ItemList.itemList.heel.ToString();
+        }
+    }
+}
